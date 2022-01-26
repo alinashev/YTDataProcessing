c3d5c8e176b417c464fa59ba851d1b861b4fb0fb
alinashev <al.t.shevchenko@gmail.com>
2022-01-26 15:33:07 +0200

    Changed lambda puller: added sending message to sqs queue

--- LambdaPuller/Commons/StorageS3.py
+++ LambdaPuller/Commons/StorageS3.py
@@ -3,12 +3,12 @@ import boto3
 
 class StorageS3:
 
-    def __init__(self, bucket_name='a-channel-data-bucket-01-10') -> None:
+    def __init__(self, bucket_name) -> None:
         self.s3 = boto3.resource('s3')
-        self.__bucket_name = bucket_name
+        self.bucket_name = bucket_name
 
     def upload(self, file_name, directory: str) -> None:
-        self.s3.meta.client.upload_file(file_name, self.__bucket_name,
+        self.s3.meta.client.upload_file(file_name, self.bucket_name,
                                         '{directory}{name}'.format(
                                             directory=directory,
                                             name=file_name.replace("tmp/", ""))
--- LambdaPuller/index.py
+++ LambdaPuller/index.py
@@ -5,11 +5,13 @@ from Commons.StorageS3 import StorageS3
 from Extract.ChannelDataExtractor import ChannelDataExtractor
 from Extract.VideoCategoryExtractor import VideoCategoryExtractor
 from Extract.VideoDataExtractor import VideoDataExtractor
+from LambdaPuller.Commons.ServiceSQS import ServiceSQS
 from Transform.VideoIDParser import VideoIDParser
 
 
 def lambda_handler(event, context):
     channel = (event['Records'][0]['body'])
+    future_message = channel
 
     folder_name = str(datetime.now().date()) + "/" + \
                   str(datetime.now().hour) + "h"
@@ -31,7 +33,7 @@ def lambda_handler(event, context):
     extractor_videos: VideoDataExtractor = VideoDataExtractor()
     video_category_extractor: VideoCategoryExtractor = VideoCategoryExtractor()
 
-    storage: StorageS3 = StorageS3(bucket_name=os.environ.get("BucketName"))
+    storage: StorageS3 = StorageS3(os.environ.get("BucketName"))
 
     channel_data = extractor_channels.extract(channel_id)
     video_data = extractor_videos.extract(channel_id)
@@ -47,4 +49,6 @@ def lambda_handler(event, context):
     storage.upload(file_writer_videos.get_path(), "Resources/" + folder_name + "/" + "videoData")
     storage.upload(file_writer_category.get_path(), "Resources/" + folder_name + "/" + "categoryData")
 
+    service: ServiceSQS = ServiceSQS()
+    service.send_message(future_message, queue_name=os.environ.get("TransformerTriggerQueueName"))
     return "Successfully completed"
